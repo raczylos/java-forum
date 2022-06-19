caed4e1f109641ff0f73333770b5d3318e645e30
szymonszymonszymonv <szymon.prokopiuk99@gmail.com>
2022-06-19 22:43:23 +0200

    useless

--- src/main/java/com/example/forum/topic/TopicController.java
+++ src/main/java/com/example/forum/topic/TopicController.java
@@ -1,8 +1,24 @@
 package com.example.forum.topic;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class TopicController {
+    private TopicService topicService;
+
+    // CREATE A TOPIC
+    @CrossOrigin(origins="http://localhost:3000")
+    @PostMapping(path="api/v1/topics/create")
+    public void createTopic() {
+
+    }
+
+
+    // REPLY TO A TOPIC
+    @CrossOrigin(origins="http://localhost:3000")
+    @PostMapping(path="api/v1/topics/{topicId}/reply")
+    public void replyToTopic(@PathVariable String topicId) {
+
+    }
     //TODO: control topics
 }
